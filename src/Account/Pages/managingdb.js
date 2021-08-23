8c4d15a1eccbbb4f356d11f3f0d1ccc6030cf3d9
G4RAYAKA <88182536+G4RAYAKA@users.noreply.github.com>
2021-08-23 20:47:59 +0530

    Manageing page finish

--- src/Account/Pages/managingdb.js
+++ src/Account/Pages/managingdb.js
@@ -17,34 +17,31 @@ function Additem() {
         <i><h1 style={{
                 position: 'absolute',
                 right: 10,
-                top: 72,}}>Inventory<br></br>Management</h1></i>
+                top: 72,}}>Manage<br></br>Database</h1></i>
                 <img
                 style={{
                   position: 'absolute',
                   left: 250,
                   top: 50,
-                width: 160,
-              height: 160}}
+                width: 160
+               }}
                 src = {logo}/>
-              <br></br>
+                <br></br>
                 <br></br><br></br><br></br>
                 <center><Card border ='primary' style={{ width: '40rem' }}>
                 <Card.Header style ={{backgroundColor: '#000428'}}><h3 style ={{color:'white'}}>Add Item</h3></Card.Header>
                 <Card.Body>
                 <form>
                     <label>
-                        Item Code : &nbsp;&nbsp;&nbsp;
+                        Name of the item : &nbsp;&nbsp;&nbsp;
                         <input type="text" name="code" />
                         </label><br></br><br></br>
                         <label>
-                        Item Name : &nbsp;&nbsp;
+                        Update : &nbsp;&nbsp;
                         <input type="text" name="name" />
                         </label><br></br><br></br>
                         <label>
-                        Supplier : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <input type="text" name="supplier" />
-                        </label><br></br><br></br>
-                        <label>
+
                         Quantity : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         <input type="text" name="quantity"/>
                         </label><br></br><br></br>
