b89d991fd15e11c078f8be89a7d2ae4a420a509e
G4RAYAKA <randikaweerawarnakula@gmail.com>
2021-08-23 13:07:20 +0530

    Account Dash

--- src/Account/Home.js
+++ src/Account/Home.js
@@ -11,9 +11,31 @@ function Home() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
-    </div>
+
+    <h1 className="h1">Account Management</h1>
+
+    <h1 className="h2">Dashboard</h1>
+<br/>
+
+    <div className="button_group">
+      <div className="space"></div><div className="space"></div><div className="space"></div><div className="space"></div><div className="space"></div><div className="space"></div><button className="button">Company Income</button><div className="space"></div>
+      <button className="button">Financial Report</button>
+</div>
+<br/>
+
+<div className="button_group">
+  <div className="space"></div><div className="space"></div><div className="space"></div><div className="space"></div><div className="space"></div><div className="space"></div><button className="button">Manage Database</button><div className="space"></div>
+  <button className="button">Cash Flow</button>
+</div>
+
+
+
+        </div>
+
+
 
   );
+
 }
 
 export default Home;
