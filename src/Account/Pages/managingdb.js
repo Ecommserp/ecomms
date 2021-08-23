024be38bf3f0f8b1db59d3179578e3f8a8d81d20
G4RAYAKA <88182536+G4RAYAKA@users.noreply.github.com>
2021-08-23 19:22:40 +0530

    managing dbv page

--- src/Account/Pages/managingdb.js
+++ src/Account/Pages/managingdb.js
@@ -1,12 +1,68 @@
 import React from 'react';
+import logo from "./cyan.png";
+import inventory from "../../assets/inventory.png"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import {motion} from 'framer-motion';
+import { animationOne, transition } from '../animations';
 
-function managingdb() {
+
+function Additem() {
   return (
-    <div className='reports'>
-      <h1>Reports</h1>
-    </div>
+    <motion.div className = "additem" initial='out'
+    animate='in'
+    exit='out'
+    variants={animationOne}
+    transition={transition}>
+        <i><h1 style={{
+                position: 'absolute',
+                right: 10,
+                top: 72,}}>Inventory<br></br>Management</h1></i>
+                <img
+                style={{
+                  position: 'absolute',
+                  left: 250,
+                  top: 50,
+                width: 160,
+              height: 160}}
+                src = {logo}/>
+                <img
+                style={{
+                  position: 'absolute',
+                right: 240,
+                top: 76,
+              width: 120,
+            height: 100}}
+                  src={inventory} /><br></br>
+                <br></br><br></br><br></br>
+                <center><Card border ='primary' style={{ width: '40rem' }}>
+                <Card.Header style ={{backgroundColor: '#000428'}}><h3 style ={{color:'white'}}>Add Item</h3></Card.Header>
+                <Card.Body>
+                <form>
+                    <label>
+                        Item Code : &nbsp;&nbsp;&nbsp;
+                        <input type="text" name="code" />
+                        </label><br></br><br></br>
+                        <label>
+                        Item Name : &nbsp;&nbsp;
+                        <input type="text" name="name" />
+                        </label><br></br><br></br>
+                        <label>
+                        Supplier : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <input type="text" name="supplier" />
+                        </label><br></br><br></br>
+                        <label>
+                        Quantity : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <input type="text" name="quantity"/>
+                        </label><br></br><br></br>
+                        <input type="submit" value="Submit"/>
+                        </form>
+                        </Card.Body></Card>
+                        </center>
+    </motion.div>
+
   );
 }
 
-export default managingdb;
+export default Additem;
