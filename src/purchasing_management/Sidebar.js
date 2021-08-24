45d77a41d70019e111e274922bee4cb4c023280b
Dinith Kaushalya <dinithkaushalya55@gmail.com>
2021-08-24 16:44:39 +0530

    css update

---
+++ src/purchasing_management/Sidebar.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';
+import { NavLink } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    <div
+      style={{ display: 'flex', height: '100.5vh',marginTop: '-20px' ,overflow: 'scroll initial' }}
+    >
+      <CDBSidebar textColor="#fff" backgroundColor="#1F78B4">
+        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+          <a
+            href="/"
+            className="text-decoration-none2"
+            style={{ color: 'inherit' }}
+          >
+            Dashboard
+          </a>
+        </CDBSidebarHeader>
+
+        <CDBSidebarContent className="sidebar-content">
+          <CDBSidebarMenu>
+            <NavLink exact to="/Purchasing_Management/pages/Search_stocks" activeClassName="activeClicked" >
+              <CDBSidebarMenuItem icon="search">Search Stocks</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/Purchasing_Management/pages/Manage_stocks" activeClassName="activeClicked" >
+              <CDBSidebarMenuItem icon="columns">Manage Stocks</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/Purchasing_Management/pages/View_stocks" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="table">View Stocks</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/Purchasing_Management/pages/View_suppliers" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="table">View Suppliers</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/Purchasing_Management/pages/reports" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="chart-line">Generate reports</CDBSidebarMenuItem>
+            </NavLink>
+
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
+
+        <CDBSidebarFooter style={{ textAlign: 'center' }}>
+          <div
+            style={{
+              padding: '20px 5px',
+            }}
+          >
+          Dinith
+          </div>
+        </CDBSidebarFooter>
+      </CDBSidebar>
+    </div>
+
+
+  );
+};
+
+export default Sidebar;
