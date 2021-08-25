7cdc62a96c8eb12d67c03e8a1e4755b1fe7d4a4c
Dinith Kaushalya <dinithkaushalya55@gmail.com>
2021-08-25 10:15:50 +0530

    plugin fix

--- src/purchasing_management/Sidebar.js
+++ src/purchasing_management/Sidebar.js
@@ -12,7 +12,7 @@ import { NavLink } from 'react-router-dom';
 const Sidebar = () => {
   return (
     <div
-      style={{ display: 'flex', height: '100.5vh',marginTop: '-20px' ,overflow: 'scroll initial' }}
+      style={{ display: 'flex', height: '100vh',marginTop: '0px' ,overflow: 'scroll initial' }}
     >
       <CDBSidebar textColor="#fff" backgroundColor="#1F78B4">
         <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
