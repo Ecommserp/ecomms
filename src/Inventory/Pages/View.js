c731424462a0058aa54b160f724ea4509fc9358b
Bimsara Wijethunga <85950261+bimsarawijethunga@users.noreply.github.com>
2021-09-20 21:50:26 +0530

    View Inventory Completed

--- src/Inventory/Pages/View.js
+++ src/Inventory/Pages/View.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {  useState } from "react";
 import logo from "./assets/logo.png";
 import './invPages.css';
 import inventory from "./assets/inventory.png";
@@ -8,7 +9,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import {motion} from 'framer-motion';
 import { animationOne, transition } from '../animations';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import MaterialTable from "material-table";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -16,61 +17,111 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TablePagination from '@material-ui/core/TablePagination';
+import { forwardRef } from 'react';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import EditIcon from '@material-ui/icons/Edit';
 import Paper from '@material-ui/core/Paper';
+import ReactDOM from 'react-dom'
+
+let table_data = [];
 
 
-  const columns = [
-    { id: 'id', label: 'Product ID', minWidth: 170 },
-    { id: 'name', label: 'Name', minWidth: 100 },
-    {
-      id: 'type',
-      label: 'Type',
-      minWidth: 170,
-      align: 'right',
-    },
-    {
-      id: 'quantity',
-      label: 'Quantity',
-      minWidth: 170,
-      align: 'right',
-    },
-  ];
-      
-      function createData(id, name, type, quantity) {
-        return { id, name, type, quantity };
-      }
-      
-      const rows = [
-        createData(1001, 'Cement', 'PURC', 500),
-        createData(1002, 'Wires', 'PURC', 400),
-        createData(1003, 'Bulbs', 'PURC', 500),
-        createData(1004, 'Banners', 'MANU', 390),
-        createData(1005, 'Wrenches', 'PURC', 600),
-        createData(1006, 'Monitors', 'MANU', 400),
-      ];
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
 
-      const useStyles = makeStyles({
-        root: {
-          width: '100%',
-        },
-        container: {
-          maxHeight: 440,
-        },
-      });
-      
-      export default function View() {
-        const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(6);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
   };
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
+
+function View() {
+
+
+
+
+async function getData(url) {
+const response = await fetch(url);
+
+return response.json();
+}
+
+async function getData_rev() {
+
+    const apiUrl = 'http://localhost:3220/inventory';
+    table_data = await getData(apiUrl);
+render_data();
+
+
+
+}
+
+
+
+
+const loaddata = document.getElementById("load_data");
+
+async function render_data() {
+
+  if(document.getElementById("load_data")) {
+    ReactDOM.render(
+  <MaterialTable
+
+
+ icons={tableIcons}
+      title="View Inventory"
+      columns={[
+        { title: 'Product ID', field: 'Product_ID' , minWidth: 200  },
+
+
+        {title: 'Product Name',field: 'Product_name', minWidth: 200},
+
+
+            { title: 'Type', field: 'Product_type', minWidth: 200  },
+              {title: 'Quantity',field: 'quantity', minWidth: 200, align: 'center' },
+      ]}
+      data={table_data}
+
+      options={{
+        search: false,
+        actionsColumnIndex: -1
+      }}
+
+    />, document.getElementById("load_data"));
+}
+}
+
+
+
+
+
+
+getData_rev();
 
         
   return (
@@ -97,52 +148,16 @@ import Paper from '@material-ui/core/Paper';
                 top: 12}}
                   src={inventory} />
                   <img style={{ width:'120px',height:'100px', marginTop:50 }} src={view} /><br></br><br></br>
-                  <Paper className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map((column) => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[6, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+                  <div id="load_data"> </div>
+
+
+        <div>
+
+            </div>
 
     </motion.div></div>
     
   );
 }
+
+export default View;
